200fd47c9d07aaff4b0c7722dcdf24553c63f87d
ttrider <vyangurs@hotmail.com>
2014-11-04 23:49:10 -0800

    adding unit tests

---
+++ PowerShellAsync/AsyncCmdlet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Management.Automation;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace TTRider.PowerShellAsync
+{
+    /// <summary>
+    /// Base class for async-enabled cmdlets
+    /// </summary>
+    public abstract class AsyncCmdlet : PSCmdlet
+    {
+        sealed protected override void BeginProcessing()
+        {
+            Async(BeginProcessingAsync);
+        }
+
+        sealed protected override void ProcessRecord()
+        {
+            Async(ProcessRecordAsync);
+        }
+
+        sealed protected override void EndProcessing()
+        {
+            Async(EndProcessingAsync);
+        }
+
+        sealed protected override void StopProcessing()
+        {
+            Async(StopProcessingAsync);
+        }
+
+        new public void WriteDebug([CanBeNull] string text)
+        {
+            //this.workItems.Add(new AsyncCommandRuntime.WorkItemAction<string> { Action = owner.WriteDebug, Argument = text });
+        }
+
+
+        protected virtual Task BeginProcessingAsync(AsyncCommandRuntime context)
+        {
+            return null;
+        }
+
+        protected virtual Task EndProcessingAsync(AsyncCommandRuntime context)
+        {
+            return null;
+        }
+
+        protected virtual Task ProcessRecordAsync(AsyncCommandRuntime context)
+        {
+            return null;
+        }
+        protected virtual Task StopProcessingAsync(AsyncCommandRuntime context)
+        {
+            return null;
+        }
+
+        private void Async(Func<AsyncCommandRuntime, Task> handler)
+        {
+            var context = new AsyncCommandRuntime(this);
+        }
+
+        
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TTRider.PowerShellAsync;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+
+    public class TestCmdlet01 : AsyncCmdlet
+    {
+        protected override Task ProcessRecordAsync(AsyncCommandRuntime context)
+        {
+            this.WriteDebug("foo");
+            return base.ProcessRecordAsync(context);
+        }
+    }
+}
