44248864ffe11f85a6677c138780ac197a456389
Charlie Kindel <tig@users.noreply.github.com>
2020-03-18 11:43:56 -0600

    converted to a .NET Core solution. See issue #5.

---
+++ UnitTests.old/Infrastructure/TestPsHostRawUserInterface.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Management.Automation.Host;
+
+namespace TTRider.PowerShellAsync.UnitTests.Infrastructure
+{
+    class TestPsHostRawUserInterface : PSHostRawUserInterface
+    {
+        public override KeyInfo ReadKey(ReadKeyOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void FlushInputBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetBufferContents(Coordinates origin, BufferCell[,] contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetBufferContents(Rectangle rectangle, BufferCell fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BufferCell[,] GetBufferContents(Rectangle rectangle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ScrollBufferContents(Rectangle source, Coordinates destination, Rectangle clip, BufferCell fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ConsoleColor ForegroundColor { get; set; }
+        public override ConsoleColor BackgroundColor { get; set; }
+        public override Coordinates CursorPosition { get; set; }
+        public override Coordinates WindowPosition { get; set; }
+        public override int CursorSize { get; set; }
+        public override Size BufferSize { get; set; }
+        public override Size WindowSize { get; set; }
+
+        public override Size MaxWindowSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Size MaxPhysicalWindowSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool KeyAvailable
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string WindowTitle { get; set; }
+    }
+}
